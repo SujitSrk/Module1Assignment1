2f11a784ff0a299ef67147ca8c2a4b58479c9227
Sujit Srk <sujit2050@hotmail.com>
2016-03-03 06:14:15 +0530

    First Commit

---
+++ app/src/main/java/com/sujitsarkar/module1assignment1/MainActivity.java
@@ -0,0 +1,88 @@
+package com.sujitsarkar.module1assignment1;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+
+
+public class MainActivity extends AppCompatActivity
+        //Implement OnClickListener for implementing listener for buttons
+        implements View.OnClickListener {
+
+    //Define Button variables
+    Button button1, button2, button3, button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Set button variables to corresponding UI widgets
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+
+        //Setup Listeners for the buttons to the context
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+
+//       Commented auto-generated code as it is not used in this excercise
+// FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View v) {
+       //Cast View to button as the event is attached to a button only
+        Button button = (Button) v;
+
+        //Get Text of the Button
+        String title = button.getText().toString();
+
+        //Print to Log
+        Log.i("Module1Assignment1","Button Pressed: "+ title);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
